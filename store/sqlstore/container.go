1cd98e34d1bbf0c5ed3b1bd2c292e59725b5e340
nick <nick@qq.com>
2025-06-06 15:43:59 +0800

    添加 GetPhoneDevice 接口

--- store/sqlstore/container.go
+++ store/sqlstore/container.go
@@ -286,3 +286,22 @@ func (c *Container) DeleteDevice(ctx context.Context, store *store.Device) error
 	_, err := c.db.Exec(ctx, deleteDeviceQuery, store.ID)
 	return err
 }
+
+const getPhoneDeviceQuery = `
+SELECT jid, lid, registration_id, noise_key, identity_key,
+       signed_pre_key, signed_pre_key_id, signed_pre_key_sig,
+       adv_key, adv_details, adv_account_sig, adv_account_sig_key, adv_device_sig,
+       platform, business_name, push_name, facebook_uuid
+FROM whatsmeow_device
+WHERE jid LIKE $1
+LIMIT 1
+`
+
+// GetAllDevices finds all the devices in the database.
+func (c *Container) GetPhoneDevice(ctx context.Context, phoneNumber string) (*store.Device, error) {
+	sess, err := c.scanDevice(c.db.QueryRow(ctx, getPhoneDeviceQuery, phoneNumber+":%"))
+	if errors.Is(err, sql.ErrNoRows) {
+		return nil, nil
+	}
+	return sess, err
+}
